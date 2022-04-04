c9879909fba7272c982b99432de999a9c8e24e3b
Gilbert Subay <gilbertttsubay@gmail.com>
2022-04-04 15:57:15 +0700

    first push

---
+++ dummy-framework-qris/dummy_framework_qris.h
@@ -0,0 +1,18 @@
+//
+//  dummy_framework_qris.h
+//  dummy-framework-qris
+//
+//  Created by Gilbert on 4/4/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for dummy_framework_qris.
+FOUNDATION_EXPORT double dummy_framework_qrisVersionNumber;
+
+//! Project version string for dummy_framework_qris.
+FOUNDATION_EXPORT const unsigned char dummy_framework_qrisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <dummy_framework_qris/PublicHeader.h>
+
+
